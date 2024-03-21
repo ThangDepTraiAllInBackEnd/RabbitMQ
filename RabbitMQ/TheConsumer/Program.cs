012626de3fd2dd87a329ac5e9b2a06129a835748
ThienThang <nguyenthienthang2002@gmail.com>
2024-03-21 23:47:52 +0700

    push

---
+++ RabbitMQ/TheConsumer/Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
+using System.Threading.Channels;
+using System.Reflection;
+Console.WriteLine("Hello, World!");
+
+
+//var factory = new ConnectionFactory
+//{
+//    HostName = "localhost",
+//    UserName = ConnectionFactory.DefaultUser,
+//    Password = ConnectionFactory.DefaultPass,
+//    Port = AmqpTcpEndpoint.UseDefaultPort,
+//};
+////Create the RabbitMQ connection using connection factory details as i mentioned above
+//var connection = factory.CreateConnection();
+////Here we create channel with session and model
+//using
+//var channel = connection.CreateModel();
+////declare the queue after mentioning name and a few property related to that
+//channel.QueueDeclare("product", exclusive: false);
+////channel.QueueDeclare("product", true, true, false, null);
+////Set Event object which listen message from chanel which is sent by producer
+//var consumer = new EventingBasicConsumer(channel);
+//consumer.Received += (model, eventArgs) =>
+//{
+//    var body = eventArgs.Body.ToArray();
+//    var message = Encoding.UTF8.GetString(body);
+//    Console.WriteLine($"Product message received: {message}");
+//};
+////read the message
+//channel.BasicConsume(queue: "product", autoAck: true, consumer: consumer);
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    Port = AmqpTcpEndpoint.UseDefaultPort,
+    UserName = ConnectionFactory.DefaultUser,
+    Password = ConnectionFactory.DefaultPass,
+    VirtualHost = "/"
+};
+
+var connection = factory.CreateConnection();
+var model = connection.CreateModel();
+
+model.ExchangeDeclare("productExchange", ExchangeType.Direct);
+model.QueueDeclare("product", true, false, false, null);
+model.QueueBind("product", "productExchange", "directexchange_key");
+
+var consumer = new EventingBasicConsumer(model);
+consumer.Received += (sender, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine("Received message: {0}", message);
+};
+
+model.BasicConsume(queue: "product",
+                          autoAck: true,
+                          consumer: consumer);
+
+Console.WriteLine("Waiting for messages. To exit press CTRL+C");
+Console.ReadKey();
---
+++ RabbitMQ/TheProducer/TheProducer/Controllers/SenMessageController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using Newtonsoft.Json;
+using System.Text;
+using System.Reflection;
+using RabbitMQ.Client.Exceptions;
+
+namespace TheProducer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SenMessageController : ControllerBase
+    {
+        [HttpPost("addproduct")]
+        public IActionResult AddProduct(string msg)
+        {
+
+            //var factory = new ConnectionFactory()
+            //{
+            //    HostName = "localhost",
+            //    UserName = ConnectionFactory.DefaultUser,
+            //    Password = ConnectionFactory.DefaultPass,
+            //    Port = AmqpTcpEndpoint.UseDefaultPort,
+            //};
+
+            ////Create the RabbitMQ connection using connection factory details as i mentioned above
+            //var connection = factory.CreateConnection();
+            ////Here we create channel with session and model
+            //using
+            //var channel = connection.CreateModel();
+
+            ////declare the queue after mentioning name and a few property related to that
+            //channel.QueueDeclare("product", exclusive: false);
+
+
+            ////Serialize the message
+            //var json = JsonConvert.SerializeObject(msg);
+            //var body = Encoding.UTF8.GetBytes(json);
+            ////put the data on to the product queue
+            //channel.BasicPublish(exchange: "", routingKey: "product", body: body);
+
+
+
+
+            //byte[] messagebuffer = Encoding.Default.GetBytes("Direct Message");
+            //
+            //Console.WriteLine("Message Sent");
+
+
+
+
+
+
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                Port = AmqpTcpEndpoint.UseDefaultPort,
+                UserName = ConnectionFactory.DefaultUser,
+                Password = ConnectionFactory.DefaultPass,
+                VirtualHost = "/"
+            };
+
+
+
+            var connection = factory.CreateConnection();
+            var model = connection.CreateModel();
+
+            //Console.WriteLine("creating exchange");
+            //// create exchange
+            //model.ExchangeDeclare("productExchange", ExchangeType.Direct);
+
+            ////---------------------------------
+            //model.QueueDeclare("product", true, false, false, null);
+            //Console.WriteLine("Creating Queue");
+
+            //// Bind Queue to Exchange
+            //model.QueueBind("product", "productExchange", "directexchange_key");
+
+            //Console.WriteLine("Creating Binding");
+
+            //Console.ReadLine();
+
+            ////---------------------------------
+            var properties = model.CreateBasicProperties();
+            properties.Persistent = false;
+
+            byte[] messagebuffer = Encoding.Default.GetBytes("Direct Message");
+            model.BasicPublish("productExchange", "directexchange_key", properties, messagebuffer);
+            Console.WriteLine("Message Sent");
+
+
+            return Ok();
+        }
+    }
+}
